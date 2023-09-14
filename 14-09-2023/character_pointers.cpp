f83b2b086651331bbf39e47d2e67b08bd6cb0c62
codeshubham <144806483+shubhamcodingblocks@users.noreply.github.com>
2023-09-14 16:06:28 +0530

    character pointers are treated differently

---
+++ 14-09-2023/character_pointers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	char ch = 'A';
+	char * chptr = &ch;
+
+	cout<< ch<<endl;
+	cout<< &ch<<endl;
+	cout<< chptr<<endl;
+
+	cout<< (int *) chptr<<endl;
+	cout<< (float *) chptr<<endl;
+	cout<< (void *) chptr<<endl;
+	return 0;
+}
