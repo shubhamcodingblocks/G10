f3aba9e7bf7acdf4026c2ed2366ce18ff8a322f8
codeshubham <144806483+shubhamcodingblocks@users.noreply.github.com>
2023-09-19 21:14:58 +0530

    functions and DMA

---
+++ 19-09-2023/inline_functions.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+inline void add(int a,int b){
+	cout<< a+b <<endl;
+}
+
+int main(){
+	int a = 11,b=12;
+	add(a,b);
+	add(a,b);
+	add(a,b);
+	add(a,b);
+	add(a,b);
+	add(a,b);
+	add(a,b);
+	add(a,b);
+	add(a,b);
+	add(a,b);
+	add(a,b);
+	add(a,b);
+	add(a,b);
+	
+	return 0;
+}
---
+++ 19-09-2023/prototype.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void add(int ,int );
+
+int main(){
+	int a=11,b=12;
+	add(a,b);
+	return 0;
+}
+void add(int a,int b){
+	cout<< a+b <<endl;
+}
