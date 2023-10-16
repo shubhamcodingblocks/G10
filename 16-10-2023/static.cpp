08f29df2a4b96a7481b6ae3b92d74f86f16a259d
codeshubham <144806483+shubhamcodingblocks@users.noreply.github.com>
2023-10-16 15:34:27 +0530

    static variables

---
+++ 16-10-2023/static.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+class Student{
+public:
+	char name[100];
+	int roll_no;
+	static int ct;
+	void set(char *n,int r){
+		ct ++;
+		strcpy(name,n);
+		roll_no = r;
+	}
+	int get_roll_no(){
+		return roll_no;
+	}
+	char * get_name(){
+		return name;
+	}
+	void display(){
+		cout<<"name "<<name<<" roll-no: "<<roll_no<<endl;
+	}
+};
+
+int Student::ct;
+
+int main(){
+	Student s1,s2,s3;
+	int ct = 10;
+	char a[] = "abc";
+	s1.set(a,123);
+	s2.set("def",234);
+	s2.set("fhi",345);
+	s1.display();
+	s2.display();
+	s3.display();
+	cout<< Student::ct <<endl;
+	return 0;
+}
